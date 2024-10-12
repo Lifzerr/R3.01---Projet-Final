856b589d0f2048cc3e17db6c880d740801dade4c
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2024-10-12 12:30:54 +0200

    Ajout d'une fonction qui genere le nav

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+    require_once('fonctions.php');
 ?>
 
 <!DOCTYPE html>
@@ -11,43 +12,7 @@
     <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.css">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                <a class="navbar-brand" href="index.php">war.net</a>
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link selected" aria-current="page" href="index.php">Accueil</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="panier.php">Panier</a>
-                </li>
-                <?php
-                    if (isset($_SESSION['login']) && isset($_SESSION['pwd'])) {
-                        echo '<li class="nav-item">';
-                        echo '<a class="nav-link" href="dashboard.php">Dashboard</a>';
-                        echo '</li>';
-                    }
-                ?>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-                <?php 
-                    if (isset($_SESSION['login']) && isset($_SESSION['pwd'])) {
-                        echo '<a href="logout.php" class="btn btn-primary ms-3" tabindex="-1" role="button" aria-disabled="true">Se déconnecter</a>';
-                    }
-                    else {
-                        echo '<a href="login.html" class="btn btn-primary ms-3" tabindex="-1" role="button" aria-disabled="true">Se connecter</a>';
-                    }
-                ?>
-            </div>
-        </div>
-    </nav>
+    <?php genererNav(); ?>
 
     <div class="container">
 
