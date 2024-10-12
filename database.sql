d6c05a4ae1dbf7b8c13b0cb4b78ffc276bfacaf1
Maxime Bourciez <maxime.bourciez@gmail.com>
2024-10-12 23:16:27 +0200

    Ajout des images Hitler car + maj database

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 09 oct. 2024 à 17:24
+-- Généré le : sam. 12 oct. 2024 à 21:14
 -- Version du serveur : 8.3.0
 -- Version de PHP : 8.2.18
 
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `article` (
   PRIMARY KEY (`id`),
   KEY `imageId` (`imageId`),
   KEY `categorieId` (`categorieId`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `article`
@@ -48,13 +48,14 @@ CREATE TABLE IF NOT EXISTS `article` (
 INSERT INTO `article` (`id`, `titre`, `description`, `prix`, `imageId`, `categorieId`, `quantiteDispo`) VALUES
 (1, 'Casque M1', 'Casque utilisé par les forces américaines durant la Seconde Guerre mondiale.', 120.00, 1, 1, 10),
 (2, 'Enigma', 'Machine de cryptage utilisée par les Allemands pour sécuriser leurs communications.', 500000.00, 2, 4, 5),
-(3, 'Panzer IV', 'Char de combat allemand, modèle très utilisé sur le front de l Est.', 300000.00, 3, 3, 2),
+(3, 'Panzer IV', 'Char de combat allemand, modèle très utilisé sur le front de l’Est.', 300000.00, 3, 3, 2),
 (4, 'Grenade Manche', 'Grenade à main utilisée par les soldats durant la guerre.', 25.00, 4, 10, 15),
 (5, 'Grenade Mk 2', 'Grenade à main standardisée des forces américaines.', 30.00, 5, 10, 20),
-(6, 'Luger', 'Pistolet semi-automatique utilisé par l armée allemande.', 500.00, 6, 2, 8),
+(6, 'Luger', 'Pistolet semi-automatique utilisé par l\'armée allemande.', 500.00, 6, 2, 8),
 (7, 'Masque à Gaz', 'Masque utilisé pour protéger contre les gaz toxiques.', 80.00, 7, 4, 12),
-(8, 'MG42', 'Mitrailleuse allemande utilisée pendant la Seconde Guerre mondiale.', 2000.00, 8, 2, 4),
-(10, 'Tank Panzer', 'Tank de la Wehrmacht, célèbre pour sa robustesse.', 250000.00, 10, 3, 1);
+(10, 'Tank Panzer', 'Tank de la Wehrmacht, célèbre pour sa robustesse.', 250000.00, 10, 3, 1),
+(11, 'Thompson 1928', 'La Thompson 1928 est une mitraillette américaine des années 1920, célèbre pour sa cadence rapide et son usage durant la Prohibition.', 75000.00, 11, 2, 3),
+(32, 'Voiture d\'Adolf Hitler', 'Voiture officielle d\'Adolf Hitler, utilisée comme symbole de pouvoir durant la Seconde Guerre mondiale.', 6000000.00, 12, 3, 1);
 
 -- --------------------------------------------------------
 
@@ -78,7 +79,6 @@ INSERT INTO `categorie` (`id`, `nom`) VALUES
 (2, 'Armes'),
 (3, 'Véhicules'),
 (4, 'Équipements'),
-(5, 'Armes'),
 (7, 'Uniformes'),
 (9, 'Décorations'),
 (10, 'Grenades');
@@ -95,7 +95,7 @@ CREATE TABLE IF NOT EXISTS `image` (
   `chemin` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `alt` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `image`
@@ -109,9 +109,9 @@ INSERT INTO `image` (`id`, `chemin`, `alt`) VALUES
 (5, 'images/grenade_mk2.jpg', 'Grenade Mk 2'),
 (6, 'images/luger.jpg', 'Luger'),
 (7, 'images/masque_gaz.jpg', 'Masque à Gaz'),
-(8, 'images/mg42.jpg', 'MG42'),
-(9, 'images/sten.jpg', 'Sten'),
-(10, 'images/tank_panzer.jpg', 'Tank Panzer');
+(10, 'images/tank_panzer.jpg', 'Tank Panzer'),
+(11, 'images/Thompson_1928.jpg', 'Thompson 1928'),
+(12, 'images/Sans titre.jpeg', 'Voiture du H');
 
 --
 -- Contraintes pour les tables déchargées
