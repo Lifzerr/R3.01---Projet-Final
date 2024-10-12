160806b6e2295c82f0cf9f6e49c8aee4aac0ee3a
Maxime BOURCIEZ <maxime.bourciez@gmail.com>
2024-10-12 11:54:00 +0200

    Ajout du nav à l'ajout d'article

--- ajout-materiel.php
+++ ajout-materiel.php
@@ -18,6 +18,36 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <a class="navbar-brand" href="index.php">war.net</a>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="panier.php">Panier</a>
+                </li>
+                <?php
+                    if (isset($_SESSION['login']) && isset($_SESSION['pwd'])) {
+                        echo '<li class="nav-item">';
+                        echo '<a class="nav-link selected" href="dashboard.php">Dashboard</a>';
+                        echo '</li>';
+                    }
+                ?>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+                <a href="logout.php" class="btn btn-primary ms-3" tabindex="-1" role="button" aria-disabled="true">Se déconnecter</a>
+            </div>
+        </div>
+    </nav>
     <div class="container mt-5">
         <h2>Ajouter un Matériel</h2>
         <form action="ajout-materiel.php" method="POST" enctype="multipart/form-data">
