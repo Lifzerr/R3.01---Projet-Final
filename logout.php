b05a0991180fb3f4846c98794f56dbace930fb7a
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2024-10-14 12:03:44 +0200

    Destruction du mdp et login quand deconnexion seulement et pas du panier

--- logout.php
+++ logout.php
@@ -3,10 +3,12 @@
     session_start ();
 
     // On détruit les variables de notre session
-    session_unset ();
+    //session_unset ();
+    unset($_SESSION['login']);
+    unset($_SESSION['pwd']);
 
     // On détruit notre session
-    session_destroy ();
+    //session_destroy (); //Pour eviter de supprimer le panier lors de la deconnexion
 
     // On redirige le visiteur vers la page d'accueil
     header ('location: index.php');
