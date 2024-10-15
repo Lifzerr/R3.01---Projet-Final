51fbb4d7585041f361c9a6e114cba8436f15cb7a
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2024-10-15 14:39:02 +0200

    Ajout d'un formulaire pour la modification d'un article

--- dashboard.php
+++ dashboard.php
@@ -1,17 +1,20 @@
-<?php 
-session_start(); 
+<?php
+session_start();
 require_once('fonctions.php');
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
+
 <body>
     <?php genererNav(); ?>
 
@@ -24,76 +27,152 @@ require_once('fonctions.php');
 
     <div class="container mt-5">
         <div id="main" class="card card-body">
-        <div class="card-header d-flex justify-content-between">
-            <h2 class="title d-md-inline-flex">Dashboard</h2>
-            <button type="button" class="btn btn-primary d-md-inline-flex mb-2" onclick="window.location.href='ajout-materiel.php'">Ajouter un article</button>
-        </div>
-        
+            <div class="card-header d-flex justify-content-between">
+                <h2 class="title d-md-inline-flex">Dashboard</h2>
+                <button type="button" class="btn btn-primary d-md-inline-flex mb-2" onclick="window.location.href='ajout-materiel.php'">Ajouter un article</button>
+            </div>
 
-        <ul id="items" class="list-group">
-            <li class="list-group-item">
-            <table class="table">
-            <thead>
-                <tr>
-                    <th class="d-none">Id</th>
-                    <th>Titre</th>
-                    <th>Description</th>
-                    <th>Prix</th>
-                    <th>Quantite</th>
-                    <th>Chemin</th>
-                    <th></th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php 
-                    require_once('fonctions.php');
 
-                    // Connection Bd
-                    $conn = connectionBD();
-                    mysqli_set_charset($conn, "utf8mb4");
+            <ul id="items" class="list-group">
+                <li class="list-group-item">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th class="d-none">Id</th>
+                                <th>Titre</th>
+                                <th>Description</th>
+                                <th>Prix</th>
+                                <th>Quantite</th>
+                                <th>Chemin</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            // Connection Bd
+                            $conn = connectionBD();
+                            mysqli_set_charset($conn, "utf8mb4");
 
-                    if ($conn->connect_error) {
-                        die("Connection failed: " . $conn->connect_error);
-                    }
+                            if ($conn->connect_error) {
+                                die("Connection failed: " . $conn->connect_error);
+                            }
 
-                    // Exécuter la requête
-                    $sql = "SELECT Article.id, Article.titre, Article.description, Article.prix, Article.quantiteDispo, Image.chemin, Image.alt, Categorie.nom AS categorie
+                            // Exécuter la requête
+                            $sql = "SELECT Article.id, Article.titre, Article.description, Article.prix, Article.quantiteDispo, Image.chemin, Image.alt, Categorie.nom AS categorie
                             FROM Article
                             LEFT JOIN Image ON Article.imageId = Image.id
                             LEFT JOIN Categorie ON Article.categorieId = Categorie.id; ";
-                    $result = $conn->query($sql);
+                            $result = $conn->query($sql);
 
-                    if (!$result) {
-                        die("Erreur lors de l'exécution de la requête : " . $conn->error);
-                    }
-                    if ($result->num_rows == 0) {
-                        echo "pas d'articles disponibles !";
-                    } 
-                ?>
-                <?php 
-                    // Display les articles
-                    foreach($result as $article) { ?>
-                <tr>
-                    <th scope="row" class="d-none"><?= $article['id']?></th>
-                    <th scope="row"><?= $article['titre']?></th>
-                    <td><?= $article['description']?></td>
-                    <td><?= $article['prix']*1?></td>
-                    <td><?= $article['quantiteDispo']?></td>
-                    <td><?= $article['chemin']?></td>
-                    <td> <button class="btn btn-warning btn-sm float-right btnModifier" data-id="<?= $article['id'] ?>">Modifier</button> </td>
-                    <td> <button class="btn btn-danger btn-sm float-right btnSupprimer" data-id="<?= $article['id'] ?>">Supprimer</button> </td>
-                </tr>
-                <?php 
-                    }
-                    $conn->close();
-                ?>
-            </tbody>
-            </table>
-        </ul>
+                            if (!$result) {
+                                die("Erreur lors de l'exécution de la requête : " . $conn->error);
+                            }
+                            if ($result->num_rows == 0) {
+                                echo "pas d'articles disponibles !";
+                            }
+                            ?>
+                            <?php
+                            // Display les articles
+                            foreach ($result as $article) { ?>
+                                <tr>
+                                    <th scope="row" class="d-none"><?= $article['id'] ?></th>
+                                    <th scope="row"><?= $article['titre'] ?></th>
+                                    <td><?= $article['description'] ?></td>
+                                    <td><?= $article['prix'] * 1 ?></td>
+                                    <td><?= $article['quantiteDispo'] ?></td>
+                                    <td><?= $article['chemin'] ?></td>
+                                    <td> <button type="button " class="btn btn-warning btn-sm float-right btnModifier" data-bs-toggle="modal" data-bs-target="#fenetreModale-<?= $article['id'] ?>" data-id="<?= $article['id'] ?>">Modifier</button> </td>
+                                    <td> <button class="btn btn-danger btn-sm float-right btnSupprimer" data-id="<?= $article['id'] ?>">Supprimer</button> </td>
+                                </tr>
+
+                                <div class="modal fade" id="fenetreModale-<?= $article['id'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-lg"> <!-- Utilisation d'une modale large -->
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="exampleModalLabel"><?= 'Modification de l\'article ' . htmlspecialchars($article['titre']) ?></h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div class="container mt-5">
+                                                    <form action="ajout-materiel.php" method="POST" enctype="multipart/form-data">
+                                                        <div class="form-group">
+                                                            <label for="titre">Titre</label>
+                                                            <input type="text" class="form-control" id="titre" name="titre" value="<?= $article['titre']?>" required>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="description">Description</label>
+                                                            <textarea class="form-control" id="description" name="description" rows="3" value="<?= $article['quantiteDispo'] ?>" required></textarea>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="prix">Prix (€)</label>
+                                                            <input type="number" step="0.01" class="form-control" id="prix" name="prix" value="<?= $article['prix']*1?>" required>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="quantiteDispo">Quantité Disponible</label>
+                                                            <input type="number" class="form-control" id="quantiteDispo" name="quantiteDispo" value="<?= $article['quantiteDispo']?>" required>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="categorie">Catégorie</label>
+                                                            <select class="form-control" id="categorie" name="categorie" required>
+                                                                <option value="Value" disabled selected></option>  <!-- ATTENTION -->
+                                                                <!-- Remplir avec les options de catégorie -->
+                                                                <?php
+                                                                $sql = "SELECT * FROM Categorie;";
+                                                                $result = $conn->query($sql);
+                                                                if (!$result) {
+                                                                    die("Erreur lors de l'exécution de la requête : " . $conn->error);
+                                                                } else if ($result->num_rows == 0) {
+                                                                    echo "<option>pas de catégories disponibles !</option>";
+                                                                } else {
+                                                                    // Afficher les options
+                                                                    foreach ($result as $categorie) {
+                                                                        echo "<option value='" . $categorie['id'] . "'>" . $categorie['nom'] . "</option>";
+                                                                    }
+                                                                }
+                                                                ?>
+                                                            </select>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="chemin">Texte alternatif à l'image</label>
+                                                            <input type="text" class="form-control" id="alt" name="alt" placeholder="Alt de l'image" required>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="image">Uploader une image</label>
+                                                            <input type="file" class="form-control-file" id="image" name="image" accept="image/*">
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <form method="post" action="dashboard.php">
+                                                    <button type="submit" class="btn btn-secondary" name="article_id" value="<?= $article['id'] ?>">Mettre à jour</button>
+                                                </form>
+                                                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Fermer</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            <?php
+                            }
+                            $conn->close();
+                            ?>
+                        </tbody>
+                    </table>
+            </ul>
         </div>
     </div>
+
     <?php genererFooter(); ?>
+    <!-- Bootstrap 4.6.2 JS and dependencies (jQuery and Popper.js) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.6.2/js/bootstrap.min.js"></script>
+    <!-- Bootstrap CSS -->
+    <!-- Bootstrap JS (with Popper.js) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="js/scriptDashboard.js"></script>
 </body>
+
 </html>
