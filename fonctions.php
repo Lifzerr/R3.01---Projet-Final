5d5c016f5891492290b2efb43725b9aa37d632b1
Maxime Bourciez <maxime.bourciez@gmail.com>
2024-10-24 18:20:43 +0200

    Fix de l'index + prise en compte png et gif pour les vignettes

--- fonctions.php
+++ fonctions.php
@@ -3,7 +3,9 @@
 function redimage($img_src, $img_dest, $dst_w, $dst_h) {
     // Lit les dimensions de l'image
     $size = GetImageSize("$img_src");
-    $src_w = $size[0]; $src_h = $size[1];
+    $src_w = $size[0]; 
+    $src_h = $size[1];
+    $mime_type = $size['mime'];
 
     // Teste les dimensions tenant dans la zone
     $test_h = round(($dst_w / $src_w) * $src_h);
@@ -12,19 +14,46 @@ function redimage($img_src, $img_dest, $dst_w, $dst_h) {
     // Crée une image vierge aux bonnes dimensions
     $dst_im = ImageCreateTrueColor($dst_w, $dst_h);
 
-    // Copie l'image initiale redimensionnée
-    $src_im = ImageCreateFromJpeg("$img_src");
+    // Choisit la fonction de création de l'image source en fonction du type MIME
+    switch($mime_type) {
+        case 'image/jpeg':
+            $src_im = ImageCreateFromJpeg($img_src);
+            break;
+        case 'image/png':
+            $src_im = ImageCreateFromPng($img_src);
+            // Préserve la transparence pour les PNG
+            imagecolortransparent($dst_im, imagecolorallocatealpha($dst_im, 0, 0, 0, 127));
+            imagealphablending($dst_im, false);
+            imagesavealpha($dst_im, true);
+            break;
+        case 'image/gif':
+            $src_im = ImageCreateFromGif($img_src);
+            break;
+        default:
+            throw new Exception("Format d'image non pris en charge : $mime_type");
+    }
 
+    // Copie l'image initiale redimensionnée
     ImageCopyResampled($dst_im, $src_im, 0, 0, 0, 0, $dst_w, $dst_h, $src_w, $src_h);
 
-    // Sauvegarde la nouvelle image
-    ImageJpeg($dst_im, "$img_dest");
+    // Sauvegarde la nouvelle image en fonction du format source
+    switch($mime_type) {
+        case 'image/jpeg':
+            ImageJpeg($dst_im, $img_dest);
+            break;
+        case 'image/png':
+            ImagePng($dst_im, $img_dest);
+            break;
+        case 'image/gif':
+            ImageGif($dst_im, $img_dest);
+            break;
+    }
 
     // Détruis les tampons
     ImageDestroy($dst_im);
     ImageDestroy($src_im);
 
-    // Return le chemin de l'image recadree
+    // Retourne le chemin de l'image redimensionnée
     return $img_dest;
 }
 
